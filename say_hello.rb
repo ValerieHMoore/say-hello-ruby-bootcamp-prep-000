596540d5399ab3b0b47267e9dc00448d0a37254e
Valerie Moore <periwinkle17@outlook.com>
2018-05-16 22:01:37 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello(phrase="Hello ", name="Ruby Programmer!")
+def say_hello(name="Ruby Programmer!")
   puts "#{phrase}#{name}"
 end
 say_hello("Ciao""Valerie")
